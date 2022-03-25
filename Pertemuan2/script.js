7908d3ce3b0e8bc4f1c988a608932735002ff5ef
Ahmad Fauzan <12050111663@students.uin-suska.ac.id>
2022-03-25 10:22:05 +0700

    Source Code Pweb Offline

---
+++ Pertemuan2/script.js
@@ -0,0 +1,19 @@
+function onClick() {
+    let h1 = document.getElementById('judul');
+    h1.innerHTML = 'Hello World';
+
+}
+
+/*
+console.log('Hello World');
+let scope lebih sempit,lebih disarankan
+scope lebih luas 
+    if (umur >= 18) {
+        console.log('Anda sudah cukup umur untuk mengakses website ini');
+    } else {
+        console.log('Anda belum cukup umur untuk mengakses website ini');
+    }
+    for(let i = 0; i < 10; i++) {
+        console.log(i);
+    }
+*/
